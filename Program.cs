c300654f6ef88bcff2542c2f5074e4dd0571c2aa
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-05 00:00:28 +0530

    First commit to the main branch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
